c630d0dc7e8694e6f5df2b4727ed8a106c1f88e8
imckissick <ian@imckissick.com>
2017-05-10 21:41:53 -0400

    Added script Connect-ExchangeOnline.ps1

---
+++ Connect-ExchangeOnline.ps1
@@ -0,0 +1,12 @@
+Import-Module MsOnline
+
+$UserCredential = Get-Credential
+
+$Global:Session = New-PSSession `
+    -ConfigurationName Microsoft.Exchange `
+    -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+    -Credential $UserCredential `
+    -Authentication Basic `
+    -AllowRedirection
+
+Import-PSSession $Session
