a2d05f6f29fb81902cba56a89462bcd1a7d11d7f
Ian McKissick <Ian@imckissick.com>
2018-06-17 21:34:02 -0400

    Updated SetOfficeChannel.cmd
    Added comments
    OfficeC2RClient.exe is actually executed now
    Previous the command was just written to the console
    Update updateChannel output to reflect actual behavior

--- SetOfficeChannel.cmd
+++ SetOfficeChannel.cmd
@@ -1,28 +1,29 @@
-:mode
-	echo off
-	title Set Office 365 Update Channel
-	mode con cols=80 lines=32
-	color 17
-	cls
+@echo off
+title Set Office 365 Update Channel
+mode con cols=80 lines=32
+color 17
+cls
 
-	goto checkForElevation
-goto :eof
-
-:checkForElevation
+:: Check to see if script is being run with Administrator privileges
+:: Script will exit if not being run as Administrator
+:checkForAdmin
 	openfiles>nul 2>&1
 
-	if %errorlevel% EQU 0 goto PrintMenu
+	if %errorlevel% EQU 0 (
+		goto :printMenu
+	)
 
-    echo.   Checking for Administrator elevation.
+	echo.
     echo.   You are not running as Administrator.
     echo.
     echo.   Please run this from an Elevated Command Prompt
-    echo.   Or Right-Click SetOfficeChannel.cmd and Select Run as Administrator
+    echo.   Or Right-Click SetOfficeChannel.cmd and Select "Run as Administrator"
     echo.
-	echo.   Press any key to continue...
+	echo.   Press any key to exit...
 	pause>nul
 goto :eof
 
+:: Displays menu of valid options for updating the Office 365 Update Channel
 :printMenu
 	cls
 	echo.
@@ -36,26 +37,33 @@ goto :eof
 	echo.                                            0. Close.
 	echo.
 
-	goto getInput
+	goto :selectChannel
 goto :eof
 
-:getInput
+:: Gets user input to select the update channel
+:: Valid options:
+::		0 = Exit
+:: 		1 = Semi-Annual Channel
+:: 		2 = Semi-Annual Channel (Targeted)
+:: 		3 = Monthly Channel
+:: 		4 = Monthly Channel (Targeted)
+:selectChannel
 	set /p option=Select an option: 
 
 	if %option% EQU 0 (
 		goto :eof
 	) else if %option% EQU 1 (
 		set channel=Broad
-		set channelname="Semi-Annual Channel"
+		set channelname=Semi-Annual Channel
 	) else if %option% EQU 2 (
 		set channel=Targeted
-		set channelname="Semi-Annual Channel (Targeted)"
+		set channelname=Semi-Annual Channel ^(Targeted^)
 	) else if %option% EQU 3 (
 		set channel=Monthly
-		set channelname="Monthly Channel"
+		set channelname=Monthly Channel
 	) else if %option% EQU 4 (
 		set channel=Insiders
-		set channelname="Monthly Channel (Targeted)"
+		set channelname=Monthly Channel ^(Targeted^)
 	) else (
 		cls
 		echo.
@@ -64,28 +72,29 @@ goto :eof
 		echo.Press any key to continue . . .
 		
         pause>nul
-        goto printMenu
+
+        goto :printMenu
 	)
 
-	goto updateChannel
+	goto :updateChannel
 goto :eof
 
-:UpdateChannel
+:: Update the Office 365 Update Channel to the channel select in :getInput
+:: This will call OfficeC2RClient.exe
+:updateChannel
 	cls
 	echo.
-	echo.Setting Office 365 Update Channel to %channelname%
+	echo.Office 365 Update Channel has been set to: %channelname%
     echo.
 
 	cd "C:\Program Files\Common Files\Microsoft Shared\ClickToRun"
-    echo start /wait OfficeC2RClient.exe /changesetting channel=%channel%
-	echo.
+    start /wait OfficeC2RClient.exe /changesetting channel=%channel%	
+	start /wait OfficeC2RClient.exe /update user
 
-	echo.Installing most recent %channelname% build now
-	echo.Please wait...
+	echo.The most recent %channelname% build is being installed now
     echo.
-	
-	echo start /wait OfficeC2RClient.exe /update user
-
-	echo.Installation complete. Press any key to exit...
+	echo.Please reboot your computer after the installation completes
+	echo.
+	echo.This script will now exit. Press any key to close the window...
 	pause>nul
 goto :eof
